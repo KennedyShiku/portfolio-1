2990d5f8134ed32c8c46413895decfc547c0574c
Kennedy <kenrichshiku@gmail.com>
2024-02-29 01:31:03 +0300

    Added Experience for md and above

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -7,8 +7,8 @@ const Projects = () => {
             <p className='text-[#999797]'>A peak into my</p>
             <h1 className='font-bold text-4xl'>Experience</h1>
         </div>
-        <div className="px-4 py-5">
-                <div className="border-2 rounded-[20px] border-solid border-[#d9d9d9] w-[400px] px-5">
+        <div className="px-4 py-5 md:flex md:justify-around">
+                <div className="border-2 rounded-[20px] border-solid border-[#d9d9d9] w-[400px] md:h-[300px] px-5">
                     <h4 className='text-center font-bold'>Frontend</h4>
                     <div className="flex justify-around py-4">
                         <ul className="pb-4">
@@ -27,7 +27,7 @@ const Projects = () => {
                         </ul>
                     </div>
                 </div>
-                <div className="border-2 rounded-[20px] border-solid border-[#d9d9d9] w-[400px] px-5 my-4">
+                <div className="border-2 rounded-[20px] border-solid border-[#d9d9d9] w-[400px] md:h-[300px] px-5 mb-4">
                     <h4 className='text-center font-bold'>Backend</h4>
                     <div className="flex justify-center py-4">
                         <ul>
